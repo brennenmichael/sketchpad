605c3f64ebd5bdffe64ca06322a827f4c6370e77
brennenmichael <kelyen@aol.com>
2022-03-04 11:09:00 -0500

    Placed canvas in the center, added grid layout.

--- sketchpad.js
+++ sketchpad.js
@@ -7,6 +7,8 @@ function makeRows() {
     }
     canvas.style.setProperty('--grid-rows', rows);
     canvas.style.setProperty('--grid-cols', rows);
+    const canvasHeight = canvas.clientHeight
+    const canvasWidth = canvas.clientWidth
     for (c = 0; c < (rows * rows); c++) {
       let cell = document.createElement("div");
       canvas.appendChild(cell).className = "grid-item";
@@ -14,22 +16,19 @@ function makeRows() {
     const gridItems = document.getElementsByClassName('grid-item')
     for (let i = 0; i < gridItems.length; i++) {
         gridItems[i].className += " blank" 
-        setDimensions(gridItems[i], rows)
+        setDimensions(gridItems[i], canvasHeight, canvasWidth, rows)
     }
   };
   
   makeRows()
 
-function setDimensions(div, rows) {
-    div.style.height = 800/rows + "px";
-    div.style.width = 800/rows + "px";
+function setDimensions(div, boxHeight, boxWidth, rows) {
+    div.style.height = boxHeight/rows + "px";
+    div.style.width = boxWidth/rows + "px";
     div.style.padding = 100/rows + "%";
 }
 
-document.addEventListener('mouseover', (e) => {
+document.addEventListener('click', (e) => {
     let element = e.target;
-    if (element.classList.contains("grid-item")) {
-        if (!element.classList.contains("filled")) element.classList.add('filled')
-        else element.classList.remove('filled')
-    }
+    if (element.matches('.grid-item.blank')) element.classList.add('filled')
 })
