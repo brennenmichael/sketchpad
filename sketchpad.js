89098b34cb1e2b3e1922f5e6f657aa3797a72276
brennenmichael <kelyen@aol.com>
2022-03-07 07:59:17 -0500

    Changed draw from mouse click to hold and drag.

--- sketchpad.js
+++ sketchpad.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("canvas")
+const gridItems = document.getElementsByClassName('grid-item');
+let isToggling = false;
 
 function makeRows() {
     rows = prompt("Enter number for square sketchpad size (Less than 100):")
@@ -13,7 +15,6 @@ function makeRows() {
       let cell = document.createElement("div");
       canvas.appendChild(cell).className = "grid-item";
     };
-    const gridItems = document.getElementsByClassName('grid-item')
     for (let i = 0; i < gridItems.length; i++) {
         gridItems[i].className += " blank" 
         setDimensions(gridItems[i], canvasHeight, canvasWidth, rows)
@@ -37,11 +38,37 @@ function clearCanvas() {
 
 document.addEventListener('click', (e) => {
     let element = e.target;
-    if (element.matches('.grid-item.blank')) element.classList.add('filled')
-})
-
-document.addEventListener('click', (e) => {
-    let element = e.target;
     if (element.matches('.clear.btn')) clearCanvas()
 })
 
+function enableToggle(e) {
+  isToggling = true;
+
+  if (e.target !== canvas) {
+    toggle(e);
+  }
+}
+
+function disableToggle() {
+  isToggling = false;
+}
+
+function toggle(e) {
+  if (isToggling === false) {
+    return;
+  }
+
+  e.target.classList.toggle('filled');
+}
+
+function draw() {
+  canvas.onmousedown = enableToggle;
+
+  for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].onmouseenter = toggle; //changes color 
+  }
+
+  canvas.onmouseup = disableToggle;
+}
+
+draw();
