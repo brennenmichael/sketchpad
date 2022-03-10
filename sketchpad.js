6f29e3129083a53df8398450a024f027ee673657
brennenmichael <kelyen@aol.com>
2022-03-10 08:55:54 -0500

    Added functional color buttons for r, g, y, b, blk

--- sketchpad.js
+++ sketchpad.js
@@ -59,7 +59,7 @@ function disableToggle() {
 
 function toggle(e) {
   if (isToggling ===false) return
-  
+
   e.target.style.setProperty('background-color', colorWell.value)
 }
 
@@ -88,5 +88,32 @@ function startup() {
   colorWell.value = defaultColor;
   colorWell.select(); //this one is just in case color well is browser incompatible
 }
+
+function RGBToHex(rgb) {
+  // Choose correct separator
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r,g,b)" into [r,g,b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  let r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+//change colorWell on button click
+document.addEventListener('click', (e) => {
+  let element = e.target;
+  let colorWell = document.querySelector("#colorWell")
+  if (element.matches(".color-select")) colorWell.value = RGBToHex(element.style.backgroundColor);
+})
 
 draw();
